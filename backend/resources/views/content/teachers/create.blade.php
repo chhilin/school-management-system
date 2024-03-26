0e63096b8ce6cafc4e06f0a85cf61125de8c40c1
Chhilin Yun <chhilinyun07@gmail.com>
2024-03-26 16:10:28 +0700

    change style

--- backend/resources/views/content/teachers/create.blade.php
+++ backend/resources/views/content/teachers/create.blade.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>Create Teacher</title>
+    <link href="https://fonts.googleapis.com/css2?family=Battambang:wght@100;300;400;700;900&display=swap" rel="stylesheet">
+
 </head>
 
 <body>
@@ -17,71 +18,71 @@
 
             <div class="row mt-4">
                 <div class="col-md-6">
-                    <label for="teacher_id" class="form-label">Teacher ID:</label>
-                    <input type="text" placeholder="Input Teacher ID" class="form-control" name="teacher_id" id="teacher_id" required>
+                    <label for="teacher_id" class="form-label">អត្តលេខ</label>
+                    <input type="text" placeholder="បញ្ចូលអត្តលេខ" class="form-control" name="teacher_id" id="teacher_id" required>
                 </div>
 
                 <div class="col-md-6">
-                    <label for="khmer_name" class="form-label">Khmer Name:</label>
-                    <input type="text" placeholder="Input Khmer Name" class="form-control" name="khmer_name" id="khmer_name" required>
+                    <label for="khmer_name" class="form-label">ឈ្មោះជាភាសាខ្មែរ</label>
+                    <input type="text" placeholder="បញ្ចូលឈ្មោះខ្មែរ" class="form-control" name="khmer_name" id="khmer_name" required>
                 </div>
 
-                <div class="col-md-6">
-                    <label for="english_name" class="form-label">English Name:</label>
-                    <input type="text" placeholder="Input English Name" class="form-control" name="english_name" id="english_name" required>
+                <div class="col-md-6 my-3">
+                    <label for="english_name" class="form-label">ឈ្មោះជាភាសាអង់គ្លេស</label>
+                    <input type="text" placeholder="បញ្ចូលឈ្មោះអង់គ្លេស" class="form-control" name="english_name" id="english_name" required>
                 </div>
 
-                <div class="col-md-6 ">
-                    <label for="gender" class="form-label">Gender:</label>
+                <div class="col-md-6 my-3">
+                    <label for="gender" class="form-label">ភេទ</label>
                     <select class="form-select" name="gender" id="gender" required>
                         <option></option>
                         <option value="ស្រី">ស្រី</option>
                         <option value="ប្រុស">ប្រុស</option>
                     </select>
                 </div>
-                <div class="col-md-6">
-                    <label for="date_of_birth" class="form-label">Date of Birth:</label>
-                    <input type="date" placeholder="Input Date of Birth" class="form-control" name="date_of_birth" id="date_of_birth" required>
+                <div class="col-md-6 ">
+                    <label for="date_of_birth" class="form-label">ថ្ងៃខែឆ្នាំកំណើត</label>
+                    <input type="date" placeholder="បញ្ចូលឆ្នាំកំណើត" class="form-control" name="date_of_birth" id="date_of_birth" required>
+                </div>
+                <div class="col-md-6 ">
+                    <label for="address" class="form-label">អាស័យដ្ឋាន</label>
+                    <input type="text" placeholder="បញ្ចូលអាស័យដ្ឋាន" class="form-control" name="address" id="address" required>
+                </div>
+                <div class="col-md-6 my-3">
+                    <label for="phone_number" class="form-label">លេខទូរស័ព្ទ</label>
+                    <input type="number" placeholder="បញូលលេខទូរស័ព្ទ" class="form-control" name="phone_number" id="phone_number" required>
+                </div>
+                <div class="col-md-6 my-3">
+                    <label for="email" class="form-label">អ៊ីមែល</label>
+                    <input type="email" placeholder="បញ្ចូលអ៊ីមែល" class="form-control" name="email" id="email" required>
                 </div>
                 <div class="col-md-6">
-                    <label for="address" class="form-label">Address:</label>
-                    <input type="text" placeholder="Input Address" class="form-control" name="address" id="address" required>
+                    <label for="major" class="form-label">មុខជំនាញ</label>
+                    <input type="text" placeholder="បញ្ចូលមុខជំនាញ" class="form-control" name="major" id="major" required>
                 </div>
                 <div class="col-md-6">
-                    <label for="phone_number" class="form-label">Phone Number:</label>
-                    <input type="number" placeholder="Input Phone Number" class="form-control" name="phone_number" id="phone_number" required>
-                </div>
-                <div class="col-md-6">
-                    <label for="email" class="form-label">Email:</label>
-                    <input type="email" placeholder="Input Email" class="form-control" name="email" id="email" required>
-                </div>
-                <div class="col-md-6">
-                    <label for="major" class="form-label">Major:</label>
-                    <input type="text" placeholder="Input Major" class="form-control" name="major" id="major" required>
-                </div>
-                <div class="col-md-6">
-                    <label for="employment_status" class="form-label">Employment Status:</label>
+                    <label for="employment_status" class="form-label">ស្ថានភាពការងារ</label>
                     <select name="employment_status" class="form-select" id="employment_status">
                         <option value=""></option>
-                        <option value="full_time">Full time</option>
-                        <option value="part_time">Part Time </option>
+                        <option value="full_time">ពេញម៉ោង</option>
+                        <option value="part_time">ក្រៅម៉ោង </option>
                     </select>
                 </div>
-                <div class="col-md-6">
-                    <label for="hire_date" class="form-label">Hire Date:</label>
-                    <input type="date" class="form-control" name="hire_date" id="hire_date">
+                <div class="col-md-6 my-3">
+                    <label for="hire_date" class="form-label">ថ្ងៃចូលធ្វើការ</label>
+                    <input type="date" class="form-control" name="hire_date" id="hire_date" placeholder="បញូល​ ថ្ងៃចូលធ្វើការ">
                 </div>
 
-                <div class="col-md-6">
-                    <label for="image" class="form-label">Image:</label>
+                <div class="col-md-6 my-3">
+                    <label for="image" class="form-label">រូបភាព</label>
                     <input type="file" name="image" id="image" class="form-control">
                 </div>
 
             </div>
             <div class="row mt-4">
                 <div class="d-flex justify-content-end gap-3">
-                    <button type="submit" class="btn btn-danger"><a href="{{ route('teachers.list')}}" class="text-decoration-none text-white">Cancel</a> </button>
-                    <button type="submit" class="btn btn-primary ">Create</button>
+                    <button type="submit" class="btn btn-secondary "><a href="{{ route('teachers.list')}}" class="text-decoration-none text-white">Cancel</a> </button>
+                    <button type="submit" class="btn btn-warning ">Create</button>
                 </div>
             </div>
         </form>
@@ -90,3 +91,8 @@
 </body>
 
 </html>
+<style>
+    body {
+        font-family: "Battambang", system-ui;
+    }
+</style>
