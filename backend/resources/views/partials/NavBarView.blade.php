3a0891a3a2276dfa3fc8d31e4aa2003f080b4fd3
Socheat <lunsocheat284@gmail.com>
2024-04-01 15:54:28 +0700

    [pull form master]

--- backend/resources/views/partials/NavBarView.blade.php
+++ backend/resources/views/partials/NavBarView.blade.php
@@ -5,7 +5,7 @@
         <img src="/DUC_profile.jpg" alt="Description of the image" style="width: 100px; height: 100px;">
     </div>
     <div class="nav-center justify-content-center align-items-center w-100" style="margin-left: 30px;">
-        <h1>School Manage System</h1>
+        <h1>DUC Management System</h1>
     </div>
 </div>
 <style>
